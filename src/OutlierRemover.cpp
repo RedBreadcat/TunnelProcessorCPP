e094172ebf8781f3b8b5c10c528ab83a0f272202
Roby <robysantoro@gmail.com>
2018-01-09 21:22:06 +1100

    Points removed if range too far, adjusted cmake so it works on WSL (but why didn't it work in the first place?)

--- src/OutlierRemover.cpp
+++ src/OutlierRemover.cpp
@@ -16,10 +16,10 @@ void OutlierRemover::RemoveOutliers(PointCloud& pc)
 	{
 		for (int j = 0; j < pc.rings[i].GetPointCount(); j++)
 		{
-			//Remove points if between 81 and 95, -95 and -81, >125 angles. And if range <=2000
+			//Remove points if between 81 and 95, -95 and -81, >125 angles. And if range <=2000 or >5000
 			float angle, range;
 			pc.rings[i].GetPointRaw(j, angle, range);
-			if (abs(angle) > 81 && abs(angle) < 95 || angle > 125 || range <= 2000)
+			if ((abs(angle) > 81 && abs(angle) < 95) || angle > 125 || range <= 2000 || range > 5000)
 			{
 				pc.rings[i].SetPointValidity(j, false);
 			}
